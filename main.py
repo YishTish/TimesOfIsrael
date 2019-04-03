6ef66e3e9d4653abcec7e0528154113fc95a966d
Yishai Landau <yishail@gmail.com>
2019-04-03 15:25:38 +0300

    run queries until 500 rows are received. Save to csv the tweets and the word count. query is now a parameter that is collected when running the program

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import sys
 import os
 import json
 from pathlib import Path
 import datetime
+import csv
 from TwitterAPI import TwitterAPI
 from dateutil.parser import parse
 from dateutil.tz import gettz
@@ -10,46 +12,98 @@ import config
 
 tzinfos = {"CST": gettz("Asia/Jerusalem")}
 
-
-SEARCH_TERM = "גנץ"
-NUMBER_OF_TWEETS = 10
-now = datetime.datetime.now()
+NUMBER_OF_TWEETS = 100
 outputDirectory = Path(os.getcwd(), "output")
 
-fileName = "%s_%d%02d%02d-%02d%02d%02d" % (SEARCH_TERM, now.year, now.month, now.day, now.hour, now.minute, now.second)
-if __name__ == '__main__':
-    api = TwitterAPI(config.API_KEY, config.API_SECRET, auth_type='oAuth2')
-    r = api.request('search/tweets', {'q': SEARCH_TERM, 'lang': 'he', 'count': 100})
 
-    # for item in r:
-    #     print(item['text'] if 'text' in item else item)
-    tweets = r.response.json()
-    firstTweetCreatedAt = tweets['statuses'][0]['created_at']
-    print("first tweet created at: %s" % firstTweetCreatedAt)
-    print(parse(firstTweetCreatedAt, tzinfos=tzinfos))
-    csvFilename = "%s.csv" % fileName
-    with open(outputDirectory / "csv" / csvFilename, "w") as outputCsv:
-        headers = "tweet_id\tcreated_at\tuser_name\tuser_handle\ttext\tmentions_handles\turls\thashtags\tfavorite_count\tretweet_count"
-        outputCsv.write(headers+"\n")
+def getfilename(search_term):
+    now = datetime.datetime.now()
+    return "%s_%d%02d%02d-%02d%02d%02d" % (search_term, now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+
+def runquery(api, term, tweetMaxId):
+    options = {
+        'q': term,
+        'lang': 'he',
+        'count': NUMBER_OF_TWEETS,
+        'result_type': 'recent'
+    }
+    if tweetMaxId > 0:
+        options['max_id'] = tweetMaxId - 1
+    r = api.request('search/tweets', options)
+    return r.response.json()
+
+
+def escapetext(text):
+    return text.replace("\t", " ").replace("\n", "  <*newline*>  ")
+
+
+def savetweetstocsv(tweets, targetFileName):
+    headers = ["tweet_id", "created_at", "user_name", "user_handle", "text", "mentions_handles", "urls", "hashtags",
+               "favorite_count", "retweet_count"]
+    full_file_path = outputDirectory / "csv" / targetFileName
+    csv_exists = os.path.exists(full_file_path)
+    with open(full_file_path, "a", newline='', encoding='utf-8') as outputCsv:
+        writer = csv.writer(outputCsv, delimiter="\t")
+        if not csv_exists:
+            writer.writerow(headers)
+        # outputCsv.write("\n")
         for tweet in tweets['statuses']:
             createdAt = parse(tweet['created_at'], tzinfos=tzinfos)
-            mentions_usernames = []
-            for user_mention in tweet['entities']['user_mentions']:
-                mentions_usernames.append(user_mention['screen_name'].encode('utf-16'))
-            tweet_urls = []
-            for url in tweet['entities']['urls']:
-                tweet_urls.append(url['url'])
-            tweet_hashtags = []
-            for hashtag in tweet['entities']['hashtags']:
-                tweet_hashtags.append(hashtag['text'].encode('utf-16'))
-            line = "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\n" % (tweet['id_str'], createdAt, tweet['user']['name'].encode('utf-16'),
-                                                                 tweet['user']['screen_name'].encode('utf-16'), tweet['text'].encode('utf-16'), mentions_usernames,
-                                                                 tweet_urls, tweet_hashtags, tweet['retweet_count'], tweet['favorite_count'])
+            mentions_usernames = [x['screen_name'] for x in tweet['entities']['user_mentions']]
+            tweet_urls = [x['url'] for x in tweet['entities']['urls']]
+            tweet_hashtags = [x['text'] for x in tweet['entities']['hashtags']]
+            line_elements = [tweet['id_str'], createdAt, tweet['user']['name'],
+                             tweet['user']['screen_name'], escapetext(tweet['text']), mentions_usernames,
+                             tweet_urls, tweet_hashtags, tweet['retweet_count'], tweet['favorite_count']]
+            writer.writerow(line_elements)
 
-            outputCsv.write(line)
-    jsonFileName = "%s.json" % fileName
-    with open(outputDirectory / "json" / jsonFileName, "w") as fileWriter:
-        fileWriter.write(json.dumps(r.response.json()))
-    print("Output written to file")
+
+def get_tweets_texts(tweets, existing_text):
+    if existing_text is None:
+        existing_text = ""
+    for tweet in tweets:
+        existing_text = "%s %s" % (existing_text, tweet['text'])
+    return existing_text
+
+
+def get_word_count(tweets_texts):
+    tweets_texts = tweets_texts.split()
+    d = {}
+    for key in tweets_texts:
+        d[key] = d.get(key, 0) + 1
+    return sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+
+if __name__ == '__main__':
+    search_term = str(input("Please type in the term you would like to search"))
+    api = TwitterAPI(config.API_KEY, config.API_SECRET, auth_type='oAuth2')
+    fileName = getfilename(search_term)
+    numOfTweetsCollected = 0
+    tweetsArray = []
+    tweetMaxId = 0
+    collect_counter = 0
+    all_tweets_text = ""
+    while numOfTweetsCollected < 500 and collect_counter < 10:
+        collect_counter += 1
+        tweets = runquery(api, search_term, tweetMaxId)
+        all_tweets_text = "%s %s" % (all_tweets_text, get_tweets_texts(tweets['statuses'], all_tweets_text))
+        if len(tweets['statuses']) == 0:
+            break
+        tweetsArray.append(tweets)
+        tweetMaxId = tweets['statuses'][len(tweets['statuses'])-1]['id']
+        numOfTweetsCollected += len(tweets['statuses'])
+        jsonFileName = "%s_%d.json" % (fileName, collect_counter)
+        open(outputDirectory / "json" / jsonFileName, "w").write(json.dumps(tweets))
+    for tweetSet in tweetsArray:
+        savetweetstocsv(tweetSet,  "%s.csv" % fileName)
+    word_count = get_word_count(all_tweets_text)
+    word_count_filename = outputDirectory / "csv" / ("%s_word_count.csv" % fileName)
+    with open(word_count_filename, "w", newline='', encoding='utf-8') as outputCsv:
+        writer = csv.writer(outputCsv, delimiter="\t")
+        for item in word_count:
+            writer.writerow(item)
+    open("tweets_text.txt", "w").write(all_tweets_text)
+    print("Output written to files output/json/%s.json and output/csv/%s.csv" % (fileName, fileName))
 
     exit(1)
